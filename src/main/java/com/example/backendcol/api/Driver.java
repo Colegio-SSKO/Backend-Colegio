ffd5a967ec33bfba1c20655baa076dbb6f88efb3
senithkay <senithkarunarathneu@gmail.com>
2023-02-03 22:41:42 +0530

    Get and Post methods added for users and teachers API

---
+++ src/main/java/com/example/backendcol/api/Driver.java
@@ -0,0 +1,30 @@
+package com.example.backendcol.api;
+
+import java.sql.*;
+
+public class Driver {
+    public static Connection getConnection(){
+
+        Connection connection = null;
+        try {
+            String user = "root";
+            String password = "";
+            String url = "jdbc:mysql://localhost:3306/colegio";
+            String jdbcDriver = "com.mysql.cj.jdbc.Driver";
+            Class.forName(jdbcDriver);
+            connection = DriverManager.getConnection(url, user, password);
+
+
+
+        }catch (SQLException sqlException){
+            System.out.println(sqlException);
+        }
+        catch (ClassNotFoundException classNotFoundException){
+            System.out.println(classNotFoundException);
+        }
+
+        return connection;
+
+    }
+
+}
--- src/main/java/com/example/backendcol/api/Resources.java
+++ src/main/java/com/example/backendcol/api/Resources.java
@@ -5,12 +5,12 @@ import java.util.ArrayList;
 public final class Resources {
     private static ArrayList<String> resources = new ArrayList<String>();
 
-    public static boolean searchResource(String req){
-        for (String res: resources) {
-            if(req.equals(res)){
-                return true;
-            }
-        }
-        return false;
-    }
+//    public static boolean searchResource(String req){
+//        for (String res: resources) {
+//            if(req.equals(res)){
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
 }
