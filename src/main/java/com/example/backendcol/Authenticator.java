724a0a920583af25e8927b1988a37292fe5cef3c
senithkay <senithkarunarathneu@gmail.com>
2023-04-26 23:46:13 +0530

    added signin JWT part

---
+++ src/main/java/com/example/backendcol/Authenticator.java
@@ -0,0 +1,75 @@
+package com.example.backendcol;
+
+import com.example.backendcol.api.JWT;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import jakarta.servlet.http.Cookie;
+
+public class Authenticator extends ApiHandler{
+
+    public JSONObject signin(Integer id, JSONObject requestObject){
+        JSONObject jsonObject = new JSONObject();
+        try {
+            Connection connection = Driver.getConnection();
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM User WHERE email = ?");
+            statement.setString(1, requestObject.getString("email"));
+
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()){
+                String enteredPassword = requestObject.getString("password");
+                String savedPassword = resultSet.getString("password");
+                if(enteredPassword.equals(savedPassword)){
+                    jsonObject.put("isError", false);
+                    jsonObject.put("message", "Authentication successful");
+
+                    //
+                    JWT jwt = new JWT();
+
+                    //setting the header and the payload
+                    jwt.putPayload("sub" , resultSet.getInt("user_id"));
+                    jwt.putPayload("name" , resultSet.getString("f_name") + " " +resultSet.getString("l_name"));
+                    jwt.putPayload("proPic" , resultSet.getString("pro_pic"));
+                    jwt.putHeader("alg" , "HS256");
+                    jwt.putHeader("typ" , "JWT");
+
+
+                    //creating the unsigned token
+                    jwt.createToken();
+                    //signing the token
+                    jwt.sign();
+                    Cookie cookie = new Cookie("jwtToken", jwt.token);
+                    cookie.setHttpOnly(true);
+                    response.addCookie(cookie);
+
+
+                }
+                else{
+                    jsonObject.put("isError", true);
+                    jsonObject.put("message", "Authentication failed. Check the Email and the Password");
+                    jsonObject.put("userName", "");
+                    jsonObject.put("userID", "");
+                    jsonObject.put("userID", "");
+                }
+            }
+            else {
+                jsonObject.put("isError", true);
+                jsonObject.put("message", "Authentication failed. Not a registered user");
+                jsonObject.put("userName", "");
+                jsonObject.put("userID", "");
+                jsonObject.put("userID", "");
+            }
+
+
+
+        }catch (Exception exception){
+            System.out.println(exception);
+        }
+        return jsonObject;
+
+    }
+
+}
--- src/main/java/com/example/backendcol/api/JWT.java
+++ src/main/java/com/example/backendcol/api/JWT.java
@@ -1,28 +1,25 @@
 package com.example.backendcol.api;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
 
 public class JWT {
-    String token = "";
 
-    String signedToken = "";
+    String secret = "This is the secret";
+
     JSONObject header = new JSONObject();
     JSONObject payload = new JSONObject();
 
-    String Signature = "";
-
-    public void putHeader(String name, String value){
-        header.put(name, value);
-    }
-
-    public void putPayload(String name, String value){
-        header.put(name, value);
-    }
+    public String token = "";
+    public void putHeader(String name, Object value){header.put(name, value);}
+    public void putPayload(String name, Object value){payload.put(name, value);}
 
     public void createToken(){
 
@@ -31,36 +28,30 @@ public class JWT {
         token = encodedHeader+ '.' + encodedPayload;
     }
 
-    public String getSign(String unsignedToken){
-        String signature = "";
-        try{
-            MessageDigest messageDigest = MessageDigest.getInstance("SHA1");
-            byte[] result = messageDigest.digest(unsignedToken.getBytes());
-
-            for (int i = 0; i < result.length; i++) {
-                signature += Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1);
-            }
-
-
-        }
-        catch (NoSuchAlgorithmException noSuchAlgorithmException){
-            System.out.println(noSuchAlgorithmException);
-        }
-        return new String(Base64.getEncoder().encodeToString(signature.getBytes()));
-    }
-
     public void sign(){
-        signedToken = token + '.' + getSign(token);
+        token += '.' + getSign(token);
     }
 
-    public boolean validateToken(String token){
-        String[] splitted = token.split(".");
-        String header = splitted[0];
-        String payload = splitted[1];
-        String withoutSign = header + '.' + payload;
-//        if(getSign(withoutSign).equals())
-        return false;
+
+    public String getSign(String unsignedToken){
+        String sign = "";
+        SecretKeySpec secretKeySpec = new SecretKeySpec(secret.getBytes(), "HmacSHA256");
+        try{
+            Mac mac = Mac.getInstance("HmacSHA256");
+            mac.init(secretKeySpec);
+
+            byte[] hmacBytes = mac.doFinal(unsignedToken.getBytes());
+            sign = Base64.getEncoder().encodeToString(hmacBytes);
+        }catch (Exception exception){
+            System.out.println(exception);
+        }
+        return sign;
     }
+
+
+
+
+
 
 
 }
